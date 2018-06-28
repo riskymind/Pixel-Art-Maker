020131948f82a5b38c1201367c77562244e02ba6
ricky <oparaiskelechi@gmail.com>
2018-06-28 08:17:10 +0100

    commented all through the functions

--- designs.js
+++ designs.js
@@ -7,6 +7,7 @@ function makeGrid() {
 	height = $("#inputHeight").val();
 	width = $("#inputWeight").val();
 
+	//maximum number of height and width 
 	if (height > 99 || width > 99 || height < 1 || width < 1) {
 		if (!error.classList.contains("error")) {
 			error.classList.toggle("error");
@@ -17,9 +18,12 @@ function makeGrid() {
 	} else {
 		error.innerText = "";
 		$('div').removeClass("error");
+
+		//creating row via the number of height 
 		for(let x = 0; x < height; x++){
 			$("#pixelCanvas").append('<tr></tr>');
 		}
+			//creating column via the number of width
 		for (let y = 0; y < width; y++ ){
 			$("#pixelCanvas tr").each(function(){
 				$(this).append('<td></td>')
@@ -29,15 +33,18 @@ function makeGrid() {
 }
 
 color = $("#colorPicker");
+//when pressed it will get the color value
 $(document).on("mousedown", "tr td", function(){
 	let colorValue = color.val();
 	$(this).css('background-color', colorValue);
+	//when drag the color will spread through
 	$('tr td').bind("mouseover", function(){
 		let colorValue = color.val();
 		$(this).css('background-color', colorValue);
 	}).mouseup(function(){
 		$('td').unbind('mouseover');
 	});
+	//method to change the background color to white
 	$('tr td').on('dblclick', function(){
 		$(this).css('background-color', '#FFFFFF')
 	});
